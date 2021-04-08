0dbf540bd595852344d56054a50a570c966fc853
Samuel Anozie <samuelcanozie@gmail.com>
2021-04-08 11:22:58 +0100

    added keyboard functionality

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ function appendCalc(value, operator) {
   var error = document.getElementById("error");
   error.innerText = ""; //clear any existing error message
   if (calc.innerText.length > 40) {
-    error.innerText = "CHARACTERS EXCEEDED"; //display error
+    error.innerText = "CHARACTERS EXCEEDED"; //display error if character exceeds
   } else {
     if (answer.innerText) {
       if (operator == 1) {
@@ -50,15 +50,43 @@ function equal() {
       eval(calc);
     } catch (err) {
       if (err instanceof SyntaxError) {
-        error.innerText = "ERROR"; //display an error message
+        error.innerText = "ERROR"; //display an error message if calculation error
       }
     } finally {
-      var calcanswer = eval(calc);
+      var calcanswer = eval(calc); //calculate
+      calcanswer =
+        Math.round((calcanswer + Number.EPSILON) * 100000000) / 100000000; //round off number to 8 decimals
     }
     if (calcanswer) {
       answer.innerHTML = calcanswer;
     }
   } else {
-    error.innerText = "NO INPUT"; //display an error message
+    error.innerText = "NO INPUT"; //display an error message if no input
   }
 }
+
+//lets make calculator work with the keyboard keys
+document.addEventListener("keydown", function (e) {
+  var isNumber = /^[0-9,.]$/i.test(e.key);
+  var isOperator = /^[7/,*,+,-]$/i.test(e.key);
+  if (isNumber) {
+    appendCalc(e.key, 0); //add to display when number pressed on keyboard
+  }
+  if (isOperator) {
+    appendCalc(e.key, 1); //add operator when press
+  }
+  if (e.key === "=" || e.key === "Enter") {
+    equal(); //calculate when = sign is pressed
+  }
+  if (
+    e.key === "Backspace" ||
+    e.key === "Delete" ||
+    e.key === "c" ||
+    e.key === "C"
+  ) {
+    clr(); //clear one line c, del or backspace is pressed
+  }
+  if (e.key === "r" || e.key === "R") {
+    reset(); //reset calculator when r is pressed
+  }
+});
